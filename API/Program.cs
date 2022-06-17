46e6dcc8f8c8788733d397c2604def7b6c5da7e4
Umudov Umud <umudau@code.edu.az>
2022-06-17 23:30:05 +0400

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,10 +8,12 @@ ConfigurationManager config = builder.Configuration;
 builder.Services.AddDbContext<DataContext>(options=>{
     options.UseSqlite(config.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -23,6 +25,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost/5001"));
 
 app.UseAuthorization();
 
